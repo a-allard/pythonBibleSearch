284bfe6d75f2082922e29a58559d9e9ab09a21cc
Allard <skunk345678hi@gmail.com>
2017-09-05 22:14:40 -0400

    New class containing all of the classes.  Will likely split up into seperate files later for now and debug is easier here.

---
+++ bible.py
@@ -0,0 +1,40 @@
+import re
+
+class verse:
+    """A class to split each verse into.  Not sure if this is really needed."""
+    _numWords=0
+    def __init__(self):
+        pass
+class chapter(verse):
+    """This is fun"""
+    _numVerses=0
+    
+    def __init__(self):
+        verse.__init__(self)
+class book(chapter):
+    
+    _numChapters=0
+    def __init__(self):
+        chapter.__init__(self)
+
+        
+class bible(book):
+    _startText='Old Testament'
+
+
+    books=[];
+    
+    def __init__(self):
+        book.__init__(self)
+
+    def load(self):
+        f=open('KJV_txt_ForPythonBreakup.txt')
+        for line in f:
+            match=re.search(self._startText,line)
+            if(match):
+                print(line)
+                break
+        for i in range(10):
+            print(f.readline())
+        f.close()
+        
