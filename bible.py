319af9c1037cc861b6f5b997d6fddbc23d48858a
Allard <skunk345678hi@gmail.com>
2017-09-08 21:12:31 -0400

    Still working.  Have the bible getting read into.  I need to figure out why everything is being read in into verses instead of breaking up into books, chapters, verses.

--- bible.py
+++ bible.py
@@ -4,57 +4,77 @@ import sys
 
 class verse:
     """A class to split each verse into.  Not sure if this is really needed."""
-    verses=[]
+    verse=[]
     def __init__(self):
         pass
     def load(self,text):
-        print(text[0])
+        #print(text)
         text=text.replace('}','').split('{')
         for i in range(len(text)):
-            self.verses.append(text[i])
-        return self.verses
+            self.verse.append(text[i])
+        #return self.verses
+    def __getitem__(self,key):
+        return self.verse[key]
+    def __setitem__(self,index,val):
+        self.verse[index]=val
 
 
 
-class chapter(verse):
+class chapter:
     """This is fun"""
     numChapters=0
     
     _numVerses=0
     _startOfChapterKey=':1}'
-    chapterText=[]
-    
+    curChapter=0
+    chapter=[]
     def __init__(self):
-        self.verse=verse()
+        self.verse=[]
     def load(self,bookText):
         startOfChapters=[]
-        chapters=re.findall('\{\d:\d}',bookText)[-1]
+        chapters=re.findall('\{\d+:\d+}',bookText)[-1]
         
-        self.numChapters=int(re.search('\d',chapters).group())
+        self.numChapters=int(re.search('{\d+',chapters).group()[1:])
+        #print(self.numChapters)
         for curChap in range(self.numChapters):
-            stringOfStart='\{'+str(curChap)+self._startOfChapterKey
+            stringOfStart='\{'+str(curChap+1)+self._startOfChapterKey
             
             match=re.search(stringOfStart,bookText)
             if(match):
-                startOfChapters.append(match.start()+len(stringOfStart))
-        
+                startOfChapters.append(match.start()+1)
+            else:
+                print(curChap)
+        i=0
         for i in range(len(startOfChapters)-1):
-            self.chapterText.append(self.verse.load(bookText[startOfChapters[i]:startOfChapters[i+1]]))
-        #self.chapterText.append(self.verse.load(bookText[startOfChapters[-1]:]))
-        
-        return self.chapterText
-    def __get__(self,index):
-        return self.chapterText[index]
+            self.chapter.append(verse())
+            #self.curChapter[i]=(i)
+            self.chapter[i].load(bookText[startOfChapters[i]:startOfChapters[i+1]])
+            #print(bookText[startOfChapters[i]:startOfChapters[i+1]][0:10])
+        #if(i>0):
+      #  print(str(i+3)+'\t'+str(self.numChapters))
+        self.chapter.append(verse())
+        #self.curChapter[i+1]=(i+1)
+        self.chapter[i+1].load(bookText[startOfChapters[-1]:])
+##        else:
+##            self.chapter.append(verse())
+##            self.chapter[i].load(bookText[startOfChapters[-1]:])
+        #print(self.chapterText[0][0][0:50])
+        #return self.verse[0:]
+    def __getitem__(self,index):
+        return self.chapter[index]
+    def __setitem__(self,index,val):
+        self.chapter[index]=val
 
     
-class book(chapter):
+class book:
     _startOfBookKey='Page \d+ '
     _oldTBooks=[]
     _newTBooks=[]
+    bookName=''
     books=None
-    bookText=[]
+    #bookText=[]
     def __init__(self):
-        self.chapter=chapter()
+        self.book=[];
 
     def load(self,bibleText,match):
         startOfText=match.start()
@@ -72,15 +92,19 @@ class book(chapter):
                 print(curBook)
         #print((startOfBooks))
         for i in range(len(startOfBooks)-1):
-            temp=(self.chapter.load(text[startOfBooks[i]:startOfBooks[i+1]]))
-            self.bookText.append(temp)
+            self.book.append(chapter())
+            self.book[i].load(text[startOfBooks[i]:startOfBooks[i+1]])
             #print(self.books[i])
-        self.bookText.append(self.chapter.load(text[startOfBooks[-1]:]))
-        return self.bookText
+        self.book.append(chapter())
+        self.book[i+1].load(text[startOfBooks[-1]:])
+        #return self.book[0:]
             
     
-    def __get__(self,key):
-        pass
+    def __getitem__(self,key):
+        return self.book[key]
+    def __setitem__(self,index,val):
+        self.book[index]=val
+
     def __findBook__(self,abrev):
         for bookName in self.books:
             match=re.search(abrev,bookName)
@@ -112,14 +136,16 @@ class bible(book):
     books=None;
     
     def __init__(self):
-        self.book=book()
+        self.bible=book()
+        #return self.bible
 
     def load(self):
         f=open('KJV_txt_ForPythonBreakupNoHeader.txt')
         bibleText=f.read()
         match=re.search(self._startText,bibleText)
         if(match):
-            self.books=self.book.load(bibleText,match)
+            #self.books=self.book.load(bibleText,match)
+            self.bible.load(bibleText,match)
         else:
             return -1;
 
