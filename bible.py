854b29ca6370c03701a8beb9cf7ab60c34f7bdb7
Allard <skunk345678hi@gmail.com>
2017-09-08 21:54:31 -0400

    finally got this working the way I want it to be.  Need to make the books indexable by giving the book name or the abeviation. Working on a dict to do this.

--- bible.py
+++ bible.py
@@ -2,13 +2,13 @@ import re
 import os
 import sys
 
-class verse:
+class Verse:
     """A class to split each verse into.  Not sure if this is really needed."""
-    verse=[]
+    
     def __init__(self):
-        pass
+        self.verse=[]
     def load(self,text):
-        print(text[0:100])
+        #print(text[0:100])
         text=text.replace('}','').split('{')
         for i in range(len(text)):
             self.verse.append(text[i])
@@ -20,16 +20,18 @@ class verse:
 
 
 
-class chapter:
+class Chapter:
     """This is fun"""
-    numChapters=0
+    
     
     _numVerses=0
     _startOfChapterKey=':1}'
     curChapter=0
-    chapter=[]
+    
     def __init__(self):
         self.verse=[]
+        self.chapter=[]
+        self.numChapters=0
     def load(self,bookText):
         startOfChapters=[]
         chapters=re.findall('\{\d+:\d+}',bookText)[-1]
@@ -46,14 +48,16 @@ class chapter:
                 print(curChap)
         i=0
         for i in range(len(startOfChapters)-1):
-            self.chapter.append(verse())
-            #self.curChapter[i]=(i)
+            self.chapter.append(Verse())
+            
             self.chapter[i].load(bookText[startOfChapters[i]:startOfChapters[i+1]])
             
-      #  print(str(i+3)+'\t'+str(self.numChapters))
-        self.chapter.append(verse())
-
-        self.chapter[i+1].load(bookText[startOfChapters[-1]:])
+        if(i>0):
+            self.chapter.append(Verse())
+            self.chapter[i+1].load(bookText[startOfChapters[-1]:])
+        else:
+            self.chapter.append(Verse())
+            self.chapter[i].load(bookText[startOfChapters[-1]:])
 
     def __getitem__(self,index):
         return self.chapter[index]
@@ -62,14 +66,16 @@ class chapter:
 
     
 class book:
-    _startOfBookKey='Page \d+ '
-    _oldTBooks=[]
-    _newTBooks=[]
-    bookName=''
-    books=None
-    #bookText=[]
+    
     def __init__(self):
+        _startOfBookKey='Page \d+ '
+        _oldTBooks=[]
+        _newTBooks=[]
+        books=None
         self.book=[];
+        self.lookUp={'gen':0,'genesis':0,
+                     '':1,'exodus':1,
+                     'lev':2,'leviticus':2}
 
     def load(self,bibleText,match):
         startOfText=match.start()
@@ -87,16 +93,23 @@ class book:
                 print(curBook)
         
         for i in range(len(startOfBooks)-1):
-            self.book.append(chapter())
+            self.book.append(Chapter())
             self.book[i].load(text[startOfBooks[i]:startOfBooks[i+1]])
             #print(self.books[i])
-        self.book.append(chapter())
+        self.book.append(Chapter())
         self.book[i+1].load(text[startOfBooks[-1]:])
         
             
     
     def __getitem__(self,key):
-        return self.book[key]
+        if(isinstance(key,str)):
+            try:
+                index=self.loopUp[key.lower()]
+            except:
+                return -1
+        elif(isinstance(key,int)):
+            index=key
+        return self.book[index]
     def __setitem__(self,index,val):
         self.book[index]=val
 
