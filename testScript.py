703166a7fbc53e3aac2170ef3b28ed83b9c8c27a
Allard <skunk345678hi@gmail.com>
2017-09-11 21:10:39 -0400

    updated for testing.

--- testScript.py
+++ testScript.py
@@ -2,10 +2,13 @@ import os
 import sys
 import getpass
 from bible import bible
+from strReferenceList import strReferenceList
 
 
 
 bible=bible()
 bible.load()
 
+##lst=strReferenceList({'a':0,'b':1,'c':2,'d':3})
+##lst.append('This is string')
 
