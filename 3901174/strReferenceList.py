6d320b1794dc33bece16c9d687a6cc6107371a43
Allard <skunk345678hi@gmail.com>
2017-09-19 23:05:36 -0400

    added comments.

--- 3901174/strReferenceList.py
+++ 3901174/strReferenceList.py
@@ -1,7 +1,10 @@
 import caselessDictionary
 
 class strReferenceList(list):
-    """This allows the user to reference a list with string keys much like a dictionaryWhile still having the list feel.  Requires a dictionary with integer values though"""
+    """This allows the user to reference a list with string keys much like a dictionary
+    While still having the list feel.
+    Requires a dictionary with integer values though.
+    Uses the caselessDictionary to simplify things.  Could be modified to use case."""
     def __init__(self,dictionary,initvals=[]):
         self.dict=caselessDictionary.caselessDictionary()
         if not isinstance(dictionary,dict):
@@ -14,7 +17,8 @@ class strReferenceList(list):
             val=dictionary[key]
             self.dict[key.lower()]=val
 
-
+# setitem setup to check the dictionary for a key if the input is not an integer.
+# Will still use an integer as normal if one is probvided
     def __setitem__(self,index,value):
         if isinstance(index,str):
             try:
@@ -22,7 +26,7 @@ class strReferenceList(list):
             except:
                 raise('Index not found in dictionary')
         list.__setitem__(self,index,value)
-
+# Similar to above but getitem instead of set.
     def __getitem__(self,index):
         if isinstance(index,str):
             try:
