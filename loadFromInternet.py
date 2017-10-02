8e5cb5e861717e1f48e619e8f047fc7534dd2a89
Andrew Allard <skunk345678hi@gmail.com>
2017-10-02 13:04:42 -0400

    Starting to add for gathering from the internet.

--- loadFromInternet.py
+++ loadFromInternet.py
@@ -7,9 +7,14 @@ import os
 class loadFromInterent:
     _site='http://www.biblegateway.com'
     def __init__(self):
-        self.connected=False
-    def __checkCnnection__(self):
+        self._connected=False
+    def __checkConection__(self):
         if not os.system('ping 8.8.8.8'):
-            self.connected=True
+            self._connected=True
         else:
-            self.connected=False
+            self._connected=False
+    def loadVersion(self,ver):
+        self.__checkConection__()
+        if not(self._connected):
+            return
+        
